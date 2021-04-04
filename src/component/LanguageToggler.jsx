73dc76f57a9837a8b2d6e588475b923286a964e5
its-Saab <musaababs@gmail.com>
2021-04-04 18:38:28 +0200

    Add LanguageToggler translation

--- src/component/LanguageToggler.jsx
+++ src/component/LanguageToggler.jsx
@@ -2,16 +2,16 @@ import { Button } from "semantic-ui-react";
 import { useTranslation } from "react-i18next";
 
 export const LanguageToggler = () => {
-	const { i18n } = useTranslation("common");
+	const { t,i18n } = useTranslation("common");
 	return (
 		<div>
 			<Button animated="fade" onClick={() => i18n.changeLanguage("en")}>
 				<Button.Content visible>En</Button.Content>
-				<Button.Content hidden>Switch</Button.Content>
+				<Button.Content hidden>{t("button-en")}</Button.Content>
 			</Button>
 			<Button animated="fade" onClick={() => i18n.changeLanguage("sv")}>
 				<Button.Content visible>Sv</Button.Content>
-				<Button.Content hidden>Switch</Button.Content>
+				<Button.Content hidden>{t("button-sv")}</Button.Content>
 			</Button>
 		</div>
 	);
