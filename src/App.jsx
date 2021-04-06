327a2b9cd39bf1cc691fe3fef87c0e2b886f6fd9
its-Saab <musaababs@gmail.com>
2021-04-06 17:00:45 +0200

    Refactor the search bar - Add pin to map

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import { Container } from "semantic-ui-react";
+import { useState, useEffect } from "react";
 import { Header } from "./component/Header";
 import { Home } from "./pages/Home";
 import { SearchResults } from "./pages/SearchResults";
+import FetchData from "./actions/FetchData";
 import "./styles/App.css";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
 function App() {
+	const [data, setData] = useState([]);
+	const [flag, setFlag] = useState(false);
+
+	useEffect(() => {
+		if (!flag) {
+			FetchData().then((res) => {
+				setData(res);
+				setFlag(true);
+			});
+		}
+	}, []);
 	return (
 		<div className="main-container">
 			<Container>
 				<Router>
 					<Header />
 					<Switch>
-						<Route component={Home} path="/" exact />
-						<Route component={SearchResults} path="/searchresults/:query" />
+						<Route render={() => <Home item={data} />} path="/" exact />
+						<Route
+							component={(props) => (
+								<SearchResults match={props.match} item={data} />
+							)}
+							path="/searchresults/:query"
+						/>
 					</Switch>
 				</Router>
 			</Container>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { ResultCards } from "../component/ResultCards";
 import { SearchForm } from "../component/SearchForm";
 
-export const Home = () => {
-
+export const Home = ({ item }) => {
+	// item.parcel_id == parcelID && item.sender == sendersName
 	return (
-		<SearchForm />
+		<div>
+			<SearchForm />
+			<br/>
+			{item.map((parcel) => (
+				<ResultCards key={parcel.id} item={parcel} />
+			))}
+		</div>
 	);
 };
