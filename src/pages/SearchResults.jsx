fc12375e043c0642105d4e51290bdc44c087cf5a
its-Saab <musaababs@gmail.com>
2021-04-06 14:28:51 +0200

    Fix indentation

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -3,19 +3,14 @@ import axios from "axios";
 import { ResultCards } from "../component/ResultCards";
 import { SearchForm } from "../component/SearchForm";
 
-
-
-
 export const SearchResults = ({ match }) => {
 	const info = match.params.query;
 	let sendersName = info.split(":")[0] || 0;
 	let parcelID = info.split(":")[1] || 0;
-	console.log("sendersName",sendersName)
-	console.log("parcelID",parcelID)
+	console.log("sendersName", sendersName);
+	console.log("parcelID", parcelID);
 
 	const [data, setData] = useState([]);
-
-
 
 	useEffect(() => {
 		const fetchedData = async () => {
@@ -30,19 +25,22 @@ export const SearchResults = ({ match }) => {
 		fetchedData();
 	}, []);
 
-  const filteredList = data.filter(item =>item.id == parcelID | item.sender == sendersName )
-  // item.parcel_id == parcelID && item.sender == sendersName
+	const filteredList = data.filter(
+		(item) => (item.id == parcelID) | (item.sender == sendersName)
+	);
+	// item.parcel_id == parcelID && item.sender == sendersName
 	return (
 		<div>
-			{ filteredList.length !== 0 && filteredList.map(parcel => (
-        <ResultCards key={parcel.id} item={parcel}/>
-	))}
-		{filteredList.length === 0 &&
-		<div>
-			<h1>No matching parcel was found</h1>
-			<SearchForm/>
-		</div>
-		}
+			{filteredList.length !== 0 &&
+				filteredList.map((parcel) => (
+					<ResultCards key={parcel.id} item={parcel} />
+				))}
+			{filteredList.length === 0 && (
+				<div>
+					<h1>No matching parcel was found</h1>
+					<SearchForm />
+				</div>
+			)}
 		</div>
 	);
 };
